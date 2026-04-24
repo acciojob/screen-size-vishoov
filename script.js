325ba0c9b86f8843ddeacd20cfc32dde7587c9a6
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-04-24 11:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const w = document.querySelector('.w');
+const h = document.querySelector('.h');
+
+window.addEventListener('resize', ()=>{
+	w.textContent = window.innerWidth;
+	h.textContent = window.innerHeight;
+})
